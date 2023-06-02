9bde05fa98691415f2b24c4e3bc6cde6f645d0bd
Juan C. Correa <j.correa.n@gmail.com>
2023-06-02 17:15:37 -0600

    Create GPTMilestonesAnalysis.R

---
+++ GPTMilestonesAnalysis.R
@@ -0,0 +1,20 @@
+setwd("/home/jc/Documents/ChatGPT from Wikipedia")
+listado <- data.frame(dir())
+library(readtext)
+library(tm)
+DirSource()
+# Get the data directory from readtext
+DATA_DIR <- system.file("extdata/", package = "readtext")
+textos <- readtext(listado$dir..)
+textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
+
+
+library(quanteda)
+Textos <- corpus(textos)
+summary(Textos)
+aja <- data.frame(summary(Textos, n = length(Textos)))
+aja$Text <- as.numeric(aja$Text)
+library(tidyverse)
+aja2 <- aja %>% arrange(Text)
+if (require(ggplot2)) ggplot(data = aja2, aes(x = Text, y = Sentences)) +
+  geom_line() + geom_point() + theme_bw()
