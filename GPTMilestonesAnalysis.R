bf068fd90ad5bf7d964fc2f8e2d3bb474b906c63
Juan C. Correa <j.correa.n@gmail.com>
2023-06-06 07:45:07 -0600

    Update GPTMilestonesAnalysis.R

--- GPTMilestonesAnalysis.R
+++ GPTMilestonesAnalysis.R
@@ -3,6 +3,7 @@ listado <- data.frame(dir())
 library(readtext)
 library(tm)
 DirSource()
+
 # Get the data directory from readtext
 DATA_DIR <- system.file("extdata/", package = "readtext")
 textos <- readtext(listado$dir..)
@@ -12,8 +13,10 @@ textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
 library(quanteda)
 Textos <- corpus(textos)
 summary(Textos)
+docvars(Textos, "Date") <- listado$dir..
 aja <- data.frame(summary(Textos, n = length(Textos)))
 aja$Text <- as.numeric(aja$Text)
+summary(Textos)
 library(tidyverse)
 aja2 <- aja %>% arrange(Text)
 if (require(ggplot2)) ggplot(data = aja2, aes(x = Text, y = Sentences)) +
