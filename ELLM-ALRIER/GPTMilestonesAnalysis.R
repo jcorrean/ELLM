8a4722e5ad8fdf0ca3595e3b8056c7cd77f72970
alrier <you@example.com>
2023-06-06 13:30:34 -0400

    Add deleted files and new untracked files

---
+++ ELLM-ALRIER/GPTMilestonesAnalysis.R
@@ -0,0 +1,34 @@
+setwd("/home/alrier/Documentos/ChatGPT/ELLM/ELLM/Data")
+listado <- data.frame(dir())
+pkg <- function(pkg){
+   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+   if (length(new.pkg)) 
+      install.packages(new.pkg, dependencies = TRUE)
+   sapply(pkg, require, character.only = TRUE)
+}
+
+packages <- c("tidyverse","readtext","raster","sf","ggspatial","cluster","factoextra",
+              "NbClust","tidyr","forecast","semTools","corrplot",
+              "corrr","haven","psych","dplyr","lavaan","readr","cvms","tm",
+              "NLP","SnowballC","RColorBrewer","wordcloud","wordcloud2",
+              "RefManageR","bibliometrix","GGally","quanteda","ggplot2",
+              "ggpubr","Factoshiny","syuzhet","RColorBrewer","tokenizers",
+              "stringr","sentimentr","stringi","stopwords","twitteR",
+              "mscstexta4r","plyr","psych","corrr","latticeExtra",
+              "semPlot","lavaan","readr","lme4","sjPlot","gvlma","Rcmdr",
+              "tidymodels","caret","lmtest","gapminder","png","rtweet","knitr")
+
+pkg(packages)
+DirSource()
+# Get the data directory from readtext
+DATA_DIR <- system.file("extdata/", package = "readtext")
+textos <- readtext(listado$dir..)
+textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
+
+Textos <- corpus(textos)
+summary(Textos)
+aja <- data.frame(summary(Textos, n = length(Textos)))
+aja$Text <- as.numeric(aja$Text)
+aja2 <- aja %>% arrange(Text)
+ggplot(data = aja2, aes(x = Text, y = Sentences)) +
+  geom_line() + geom_point() + theme_bw()
