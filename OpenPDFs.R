bad4bf77e2020c01f8705ad6cdcc42e5d6a9e573
Juan C. Correa <j.correa.n@gmail.com>
2023-07-05 10:15:01 -0600

    Create OpenPDFs.R

---
+++ OpenPDFs.R
@@ -0,0 +1,9 @@
+setwd("/home/jc/Documents/GitHub/ELLM/ELLM-ALRIER/Datas")
+listado <- data.frame(dir())
+library(readtext)
+library(tm)
+DirSource()
+# Get the data directory from readtext
+DATA_DIR <- system.file("extdata/", package = "readtext")
+textos <- readtext(listado$dir..)
+textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
