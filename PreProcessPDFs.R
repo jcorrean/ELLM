02ad3aeeefe0c3a56efef8bd995cb668b2162b18
Juan C. Correa <j.correa.n@gmail.com>
2023-07-05 15:38:18 -0600

    Update PreProcessPDFs.R

--- PreProcessPDFs.R
+++ PreProcessPDFs.R
@@ -3,24 +3,51 @@ library(stringr)
 # Extract the hour, day, month, and year from the string
 textos$hour <- str_sub(textos$doc_id, start = 1, end = 2)
 textos$minute <- str_sub(textos$doc_id, start = 3, end = 4)
-textos$date <- str_sub(textos$doc_id, start = 5)
-textos$date <- as.Date(textos$date, format = "%d-%m-%Y")
-library(quanteda)
-Textos <- corpus(textos$text)
-docvars(Textos, "text") <- textos$text
-docvars(Textos, "Date") <- textos$date
-docvars(Textos, "Hour") <- textos$hour
-docvars(Textos, "Minute") <- textos$minute
-summary(Textos)
-versions <- data.frame(summary(Textos, n = length(Textos)))
+textos$Date <- str_sub(textos$doc_id, start = 5)
+textos$Date <- as.Date(textos$Date, format = "%d-%m-%Y")
+library(tidyr)
+December2022 <- textos %>% filter(, grepl("-12-",Date)) 
+December2022$Month <- "December"
+January2023 <- textos %>% filter(, grepl("-01-", Date))
+January2023$Month <- "January"
+February2023 <- textos %>% filter(, grepl("-02-",Date))
+February2023$Month <- "February"
+March2023 <- textos %>% filter(, grepl("-03-",Date))
+March2023$Month <- "March"
+April2023 <- textos %>% filter(, grepl("-04-",Date))
+April2023$Month <- "April"
+May2023 <- textos %>% filter(, grepl("-05-",Date))
+May2023$Month <- "May"
 
 # By looking at the data, we noticed we had some typos 
 # in the name of the following rows, so we edit them manually
 # to make them coincide with their original dates
-versions$Date[69] <- "2022-12-06"
-versions$Date[72] <- "2022-12-06"
-versions$Date[79] <- "2022-12-06"
-versions$Date[83] <- "2022-12-06"
-versions$Date[94] <- "2022-12-06"
-versions$Date[478] <- "2022-12-06"
+textos$Date[69] <- "2022-12-06"
+textos$Date[72] <- "2022-12-06"
+textos$Date[79] <- "2022-12-06"
+textos$Date[83] <- "2022-12-06"
+textos$Date[94] <- "2022-12-06"
+textos$Date[478] <- "2022-12-06"
 
+library(quanteda)
+History <- list(December2022, January2023, February2023, March2023, April2023, May2023)
+History <- do.call(rbind, History)
+Textos <- corpus(History$text)
+docvars(Textos, "text") <- textos$text
+docvars(Textos, "Date") <- textos$date
+docvars(Textos, "Hour") <- textos$hour
+docvars(Textos, "Minute") <- textos$minute
+
+
+
+
+
+textos <- data.frame(summary(Textos, n = length(Textos)))
+
+
+
+
+
+History <- list(December2022, January2023, February2023, March2023, April2023, May2023)
+History <- do.call(rbind, History)
+docvars(History, "Month") <- History$Month
