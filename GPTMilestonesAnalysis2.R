88348c2dc1109fe20f6c066358738e57907788dc
Juan C. Correa <j.correa.n@gmail.com>
2023-07-21 11:24:39 -0600

    Update GPTMilestonesAnalysis2.R

--- GPTMilestonesAnalysis2.R
+++ GPTMilestonesAnalysis2.R
@@ -11,3 +11,13 @@ topfeatures(ave2, 50)
 # Let's split the corpus into pieces of information
 # by month, as follows
 December <- corpus_subset(Textos, Month == "December")
+Dec <- tokens(December, remove_numbers = TRUE, remove_punct = TRUE) %>%  dfm()
+Dec2 <- dfm(Dec, remove = stopwords("english"))
+topfeatures(Dec2, 50) 
+
+January <- corpus_subset(Textos, Month == "January")
+February <- corpus_subset(Textos, Month == "February")
+March <- corpus_subset(Textos, Month == "March")
+April <- corpus_subset(Textos, Month == "April")
+May <- corpus_subset(Textos, Month == "May")
+
