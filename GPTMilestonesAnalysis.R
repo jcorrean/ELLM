fc99d1e6705a384ea53d37762d7f83e3b496a9cc
Juan C. Correa <j.correa.n@gmail.com>
2023-07-20 19:34:29 -0600

    Update GPTMilestonesAnalysis.R

--- GPTMilestonesAnalysis.R
+++ GPTMilestonesAnalysis.R
@@ -1,27 +1,11 @@
-setwd("/home/jc/Documents/GitHub/ELLM/ELLM-ALRIER/Datas")
-listado <- data.frame(dir())
-# Original file names should be changed as follows:
-# Instead of "ChatGPT1.pdf" it should be the
-# "December52022.pdf" or as it is more convenient
-# to analyze the series with the best time format
-# Time formats in R is a topic I don't know anything
-# about, so we need to understand that.
-library(readtext)
-library(tm)
-DirSource()
-
-# Get the data directory from readtext
-DATA_DIR <- system.file("extdata/", package = "readtext")
-textos <- readtext(listado$dir..)
-textos$doc_id <- gsub("[^0-9-]", "", textos$doc_id)
-
+setwd("/home/jc/Documents/GitHub/ELLM/")
+load("~/Documents/GitHub/ELLM/StructuredData.RData")
 
 library(quanteda)
-Textos <- corpus(textos$text)
+Textos <- corpus(History$text)
 summary(Textos)
-docvars(Textos, "Date") <- listado$dir..
+docvars(Textos, "Date") <- History$Date
 aja <- data.frame(summary(Textos, n = length(Textos)))
-aja$Text <- as.numeric(aja$Text)
 summary(Textos)
 library(tidyverse)
 aja2 <- aja %>% arrange(Text)
