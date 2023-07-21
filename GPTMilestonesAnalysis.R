514c019f41707354084466ac0b5d6a50bbde585e
Juan C. Correa <j.correa.n@gmail.com>
2023-07-21 10:52:43 -0600

    Update GPTMilestonesAnalysis.R

--- GPTMilestonesAnalysis.R
+++ GPTMilestonesAnalysis.R
@@ -7,7 +7,10 @@ summary(Textos)
 docvars(Textos, "Date") <- History$Date
 aja <- data.frame(summary(Textos, n = length(Textos)))
 summary(Textos)
-library(tidyverse)
-aja2 <- aja %>% arrange(Text)
-if (require(ggplot2)) ggplot(data = aja2, aes(x = Text, y = Sentences)) +
-  geom_line() + geom_point() + theme_bw()
+if (require(ggplot2)) ggplot(data = aja, aes(x = Date, y = Sentences)) +
+  geom_line() + geom_point() + theme_bw() + 
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "1 month") +
+  theme(axis.text.x = element_text(size = 15, color = "black"),  
+        axis.text.y = element_text(size = 15, color = "black"),  
+        axis.title.x = element_text(size = 15, color = "black"), 
+        axis.title.y = element_text(size = 15, color = "black")) 
