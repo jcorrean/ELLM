0571dc097f092d98ffbda65c1f3eac7bbf7559f3
Juan C. Correa <j.correa.n@gmail.com>
2023-07-24 13:56:06 -0600

    Update PreProcessingNetworkData.R

--- PreProcessingNetworkData.R
+++ PreProcessingNetworkData.R
@@ -1,20 +1,23 @@
 load("/home/jc/Documents/GitHub/ELLM/SelectedKeywords.RData")
 df$keyword <- tolower(df$keyword)
 Network <- df[,c(7,8)]
+network <- Network[!duplicated(Network[c(1,2)]),]
 
 library(igraph)
-bn2 <- graph.data.frame(Network,directed=FALSE)
+bn2 <- graph.data.frame(network, directed = FALSE)
 bipartite.mapping(bn2)
 V(bn2)$type <- bipartite_mapping(bn2)$type
 V(bn2)$color <- ifelse(V(bn2)$type, "red", "green")
 V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
 V(bn2)$label.cex <- ifelse(V(bn2)$type, 0.8, 1)
-#V(bn2)$size <- sqrt(igraph::degree(bn2))
 E(bn2)$color <- "lightgrey"
-plot(bn2, 
-     vertex.label = NA, 
-     layout = layout_as_bipartite, 
-     main = "")
+
+# Plot the graph using plot
+plot(bn2, vertex.label = NA, layout = layout_as_bipartite, main = "")
+
+# Rotate the plot 90 degrees clockwise
+tkplot_rotate(90)
+
 
 table(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
 mean(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
@@ -33,16 +36,29 @@ MonthsCentrality <- data.frame(Degree = degree(Months),
 Keywords <- bn2.pr$proj1
 KeywordsCentrality <- data.frame(Degree = degree(Keywords),
                                Closeness = closeness(Keywords),
-                               Betweennes = betweenness(Keywords),
+                               Betweenness = betweenness(Keywords),
                                Eigen = eigen_centrality(Keywords))
 KeywordsCentrality <- KeywordsCentrality[1:4]
+KeywordsCentrality$Term <- rownames(KeywordsCentrality)
+Top10Betweenness <- head(KeywordsCentrality[order(-KeywordsCentrality$Betweenness), ], 10)
+Top10Closeness <- head(KeywordsCentrality[order(-KeywordsCentrality$Closeness), ], 10)
+Top10Eigenvector <- head(KeywordsCentrality[order(-KeywordsCentrality$Eigen.vector), ], 10)
 
+library(ggplot2)
+ggplot(KeywordsCentrality, aes(x=reorder(Term, Eigen.vector), y=Eigen.vector)) +
+  geom_bar(stat = "identity", fill="lightgreen") + theme_bw() + 
+  theme(axis.text.x=element_text(size=15, colour="black")) +
+  theme(axis.text.y=element_text(size=15, colour="black")) +
+  theme(axis.title.x=element_text(face="italic", colour="black", size=25)) +
+  theme(axis.title.y=element_text(face="italic", colour="black", size=25)) +
+  coord_flip() + xlab("Keywords") + ylab("Eigenvector Centrality (rescaled 0-1)")
 
+# we spot a list of 26 keywords, so let's use them to depict  
+# a bipartite network with them
 
+TopEigenvector <- head(KeywordsCentrality[order(-KeywordsCentrality$Eigen.vector), ], 26)
+load("/home/jc/Documents/GitHub/ELLM/StructuredData.RData")
 
-#Network <- Network[!duplicated(Network[c(1,2)]),]
-
-load("StructuredData.RData")
 library(quanteda)
 TOKS <- corpus(History$text) %>% 
   tokens(remove_numbers = TRUE, 
@@ -52,20 +68,7 @@ TOKS <- corpus(History$text) %>%
   tokens_remove(stopwords("en"))
 DTM <- dfm(TOKS, tolower = TRUE)
 
-keywords <- c("ai", "api", "algorithm", "prompt", "architecture",
-"bias","chat","chatbot","data","hallucination","information","knowledge",
-"prototype","platform","plagiarism","misinformation","jailbreak","risk",
-"jobs","cybersecurity","algorithmic bias",
-"application programming interface", "language model",
-"proximal policy optimization", "large language model", 
-"natural language","generative pre-trained transformer ",
-"machine learning", "supervised learning", "unsupervised learning", 
-"reinforcement learning","reward models", "bing chat", "cloud computing", 
-"human intelligence", "virtual assistant", "apple","android", "baidu",
-"expedia", "facebook", "freemium", "google", "hugging face", "ios",
-"meta", "microsoft", "microsoft azure", "naver", "nvidia", 
-"opentable", "openai", "reddit", "snapchat", "shopify", "wolfram",
-"yandex", "zapier", "goodhart's law") 
+keywords <- TopEigenvector$Term
 
 toks_inside <- tokens_keep(TOKS, pattern = keywords, window = 0)
 DTM2 <- dfm(toks_inside)
@@ -96,7 +99,7 @@ plotweb(DTM3, method = "normal",
 
 
 library(igraph)
-bn2 <- graph.data.frame(Network,directed=FALSE)
+bn2 <- graph.data.frame(network,directed=FALSE)
 bipartite.mapping(bn2)
 V(bn2)$type <- bipartite_mapping(bn2)$type
 V(bn2)$color <- ifelse(V(bn2)$type, "red", "green")
@@ -106,7 +109,7 @@ V(bn2)$size <- sqrt(igraph::degree(bn2))
 E(bn2)$color <- "lightgrey"
 plot(bn2, 
      vertex.label = NA, 
-     layout = layout_components, 
+     layout = layout_as_bipartite, 
      main = "")
 
 table(Network$keyword)
@@ -114,30 +117,17 @@ Network <- Network[!duplicated(Network[c(1,2)]),]
 
 
 library(igraph)
-bn2 <- graph.data.frame(Network,directed=FALSE)
+bn2 <- graph.data.frame(network, directed = FALSE)
 bipartite.mapping(bn2)
 V(bn2)$type <- bipartite_mapping(bn2)$type
 V(bn2)$color <- ifelse(V(bn2)$type, "red", "green")
 V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
-V(bn2)$label.cex <- ifelse(V(bn2)$type, 0.5, 1)
-V(bn2)$size <- sqrt(igraph::degree(bn2))
+V(bn2)$label.cex <- ifelse(V(bn2)$type, 0.8, 1)
 E(bn2)$color <- "lightgrey"
 
-bn2.pr <- bipartite.projection(bn2)
-Terms <- bn2.pr$proj2
+# Create a layout for the graph with the desired rotation
+layout <- layout_as_bipartite(bn2)
+rotated_layout <- cbind(layout[, 2], -layout[, 1])  # Swap x and y coordinates and negate y
 
-centrality_scores <- degree(Terms)
-
-# Normalize the centrality scores to a range between 0 and 1
-normalized_scores <- (centrality_scores - min(centrality_scores)) / (max(centrality_scores) - min(centrality_scores))
-
-# Create a color palette with different colors
-color_palette <- colorRampPalette(c("red", "pink", "lightgreen", "green"))(length(unique(normalized_scores)))
-
-# Assign colors to nodes based on their normalized centrality scores
-node_colors <- color_palette[rank(normalized_scores)]
-
-# Plot the network with node colors based on centrality
-plot(Terms, vertex.label.color = "black", vertex.label.cex = 0.8, vertex.color = node_colors, vertex.size = 15, edge.width = 0.5, edge.color = "lightgray", layout = layout_components, main = "")
-
-
+# Plot the graph with the rotated layout and vertex labels
+plot(bn2, vertex.label = V(bn2)$name, layout = rotated_layout, main = "")
