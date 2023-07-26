1d12ff843d4c8da72fb0ab1605c792990930db9d
Juan C. Correa <j.correa.n@gmail.com>
2023-07-26 00:24:24 -0600

    Supplementary Material

---
+++ AlluvialPlot.R
@@ -0,0 +1,32 @@
+library(alluvial)
+source("/home/jc/Documents/GitHub/ELLM/PreProcessingNetworkData.R")
+library(dplyr)
+WF <- Network %>%
+  group_by(pattern, Month) %>%
+  count()
+colnames(WF)[3] <- "Frequency"
+WF <- WF %>%
+  mutate(Month = case_when(
+    Month == "December 2022" ~ 1,
+    Month == "January 2023" ~ 2,
+    Month == "February 2023" ~ 3,
+    Month == "March 2023" ~ 4,
+    Month == "April 2023" ~ 5,
+    Month == "May 2023" ~ 6,
+    TRUE ~ NA_integer_  # Add a default value in case none of the conditions match
+  ))
+
+unique(WF$pattern)
+
+library(alluvial)
+set.seed(987654) # for nice colours
+num_colors <- 53
+cols <- hsv(h = runif(num_colors), s = runif(num_colors, 0.2, 1), v = runif(num_colors, 0.2, 1))
+alluvial_ts(WF, wave = .3, ygap = 5, col = cols, plotdir = 'centred', alpha=.9,
+            grid = TRUE, grid.lwd = 5, xmargin = 0.2, lab.cex = .7, xlab = '',
+            ylab = '', border = NA, axis.cex = .8, leg.cex = .7,
+            leg.col='white', 
+            title = "Keywords associated with Chat GPT")
+
+unique(WF$pattern)
+length(unique(WF$pattern))
--- PreProcessingNetworkData.R
+++ PreProcessingNetworkData.R
@@ -2,126 +2,73 @@ load("/home/jc/Documents/GitHub/ELLM/SelectedKeywords.RData")
 df$keyword <- tolower(df$keyword)
 Network <- df[,c(7,8)]
 network <- Network[!duplicated(Network[c(1,2)]),]
+rownames(network) <- 1:243
 
 table(Network$pattern)
 table(Network$Month)
 table(network$pattern)
 table(network$Month)
 
-library(igraph)
-bn2 <- graph.data.frame(network,directed=FALSE)
-igraph::degree(bn2)
-bipartite.mapping(bn2)
-V(bn2)$type <- bipartite_mapping(bn2)$type
-V(bn2)$color <- ifelse(V(bn2)$type, "red", "green")
-V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
-#V(bn2)$label.cex <- ifelse(V(bn2)$type, 0.8, 1)
-V(bn2)$size <- 3.5
-E(bn2)$color <- "grey"
-plot(bn2, 
-     vertex.label = NA, 
-     layout = layout_nicely, 
-     main = "")
-table(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
-mean(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
-var(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
-min(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
-max(igraph::degree(bn2,v=V(bn2)[type==FALSE]))
-edge_density(bn2)
+library(dplyr)
 
-bn2.pr <- bipartite.projection(bn2)
-Months <- bn2.pr$proj2
-MonthsCentrality <- data.frame(Degree = degree(Months),
-                               Closeness = closeness(Months),
-                               Betweennes = betweenness(Months),
-                               Eigen = eigen_centrality(Months))
+# Find terms that did not appear in all six months
+terms <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months < 6) 
 
-Keywords <- bn2.pr$proj1
-KeywordsCentrality <- data.frame(Degree = degree(Keywords),
-                               Closeness = closeness(Keywords),
-                               Betweenness = betweenness(Keywords),
-                               Eigen = eigen_centrality(Keywords))
-KeywordsCentrality <- KeywordsCentrality[1:4]
-KeywordsCentrality$Term <- rownames(KeywordsCentrality)
-Top10Betweenness <- head(KeywordsCentrality[order(-KeywordsCentrality$Betweenness), ], 10)
-Top10Closeness <- head(KeywordsCentrality[order(-KeywordsCentrality$Closeness), ], 10)
-Top10Eigenvector <- head(KeywordsCentrality[order(-KeywordsCentrality$Eigen.vector), ], 10)
+# Find terms appearing only once
+terms1 <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months == 1) 
 
-library(ggplot2)
-ggplot(KeywordsCentrality, aes(x=reorder(Term, Eigen.vector), y=Eigen.vector)) +
-  geom_bar(stat = "identity", fill="lightgreen") + theme_bw() + 
-  theme(axis.text.x=element_text(size=15, colour="black")) +
-  theme(axis.text.y=element_text(size=15, colour="black")) +
-  theme(axis.title.x=element_text(face="italic", colour="black", size=25)) +
-  theme(axis.title.y=element_text(face="italic", colour="black", size=25)) +
-  coord_flip() + xlab("Keywords") + ylab("Eigenvector Centrality (rescaled 0-1)")
+# Find terms appearing only twice
+terms2 <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months == 2) 
 
-# we spot a list of 26 keywords, so let's use them to depict  
-# a bipartite network with them
+# Find terms appearing three times
+terms3 <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months == 3) 
 
-TopEigenvector <- head(KeywordsCentrality[order(-KeywordsCentrality$Eigen.vector), ], 26)
-load("/home/jc/Documents/GitHub/ELLM/StructuredData.RData")
+# Find terms appearing four times
+terms4 <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months == 4) 
 
-library(quanteda)
-TOKS <- corpus(History$revision) 
-docvars(TOKS, "Date") <- History$Date
-summary(TOKS)
+# Find terms appearing five times
+terms5 <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months == 5) 
 
-TOKS2 <- TOKS %>% tokens(remove_numbers = TRUE, 
-      remove_punct = TRUE, 
-      remove_url = TRUE,
-      remove_symbols = TRUE) %>% 
-      tokens_remove(stopwords("en"))
+# Find terms appearing six times
+terms6 <- network %>%
+  group_by(pattern) %>%
+  summarise(unique_months = n_distinct(Month)) %>%
+  filter(unique_months == 6) 
 
 
-class(TOKS2)
-
-DTM <- dfm(TOKS2, tolower = TRUE)
-
-keywords <- TopEigenvector$Term
-
-toks_inside <- tokens_keep(TOKS2, pattern = keywords, window = 0)
-DTM2 <- dfm(toks_inside)
-summary(DTM2)
-
-DTM3 <- as.matrix(DTM2)
-colnames(DTM3)
-row_names <- c(rep("December 2022", 203),
-               rep("January 2023", 40),
-               rep("February 2023", 47),
-               rep("March 2023", 94),
-               rep("April 2023", 42),
-               rep("May 2023", 56))
-
-rownames(DTM3) <- row_names
-
-library(bipartite)
-plotweb(DTM3, method = "normal", 
-        col.high = "lightgreen", 
-        bor.col.high = "lightgreen",
-        col.low = "pink", 
-        bor.col.low = "pink",
-        col.interaction = "grey90",
-        bor.col.interaction = "grey90",
-        low.lablength = 0,
-        labsize = 2)
-
-
-## Right at this moment, we noticed that the 
-# only terms included in the bipartite visualization
-# were unigrams, so bigrams and trigrams are not
-# present. We need an extra work to solve that problem
-
-
-
+network2 <- network %>% filter(, grepl('facebook|apple|cloud computing|human intelligence|jobs|architecture|bing chat|nvidia|opentable|shopify|wolfram|zapier|expedia|hugging face|application programming interface|platform|natural language|algorithm|meta|naver|reddit|snapchat|yandex|baidu|large language model|jailbreak|ai|openai', pattern))
 
 library(igraph)
-bn2 <- graph.data.frame(network, directed = FALSE)
+bn2 <- graph.data.frame(network2, directed = FALSE)
 bipartite.mapping(bn2)
 V(bn2)$type <- bipartite_mapping(bn2)$type
-V(bn2)$color <- ifelse(V(bn2)$type, "red", "green")
-V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
-V(bn2)$label.cex <- ifelse(V(bn2)$type, 4, 4)
+
+# Set the color and shape of the vertices based on the 'type'
+V(bn2)$color <- ifelse(V(bn2)$type, "lightblue1", "#5464C8")
+V(bn2)$shape <- ifelse(V(bn2)$type, "none", "none")
+
+# Set the label size for the vertices (you can adjust this as needed)
+V(bn2)$label.cex <- ifelse(V(bn2)$type, 1, 1)
+
+# Set the color of the edges
 E(bn2)$color <- "lightgrey"
 
 # Create a layout for the graph with the desired rotation
@@ -129,19 +76,6 @@ layout <- layout_as_tree(bn2)
 rotated_layout <- cbind(layout[, 2], -layout[, 1])  # Swap x and y coordinates and negate y
 
 # Plot the graph with the rotated layout and vertex labels
-plot(bn2, vertex.label = V(bn2)$name, layout = rotated_layout, main = "")
+plot(bn2, vertex.label = V(bn2)$name, layout = rotated_layout, main = "",
+     vertex.label.color = ifelse(V(bn2)$shape == "circle", "red", "black"))
 
-bn2 <- graph.data.frame(network, directed = FALSE)
-bipartite.mapping(bn2)
-V(bn2)$type <- bipartite_mapping(bn2)$type
-V(bn2)$color <- ifelse(V(bn2)$type, "red", "green")
-V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
-V(bn2)$label.cex <- ifelse(V(bn2)$type, 3, 1)
-E(bn2)$color <- "lightgrey"
-
-# Create a layout for the graph with the desired rotation
-layout <- layout_as_tree(bn2)
-rotated_layout <- cbind(layout[, 2], -layout[, 1])  # Swap x and y coordinates and negate y
-
-# Plot the graph with the rotated layout, vertex labels, and smaller vertex size
-plot(bn2, vertex.label = V(bn2)$name, layout = rotated_layout, main = "", vertex.size = 5)
