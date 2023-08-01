273dd96603eebc9e1bf067e9558607be86948edd
Juan C. Correa <j.correa.n@gmail.com>
2023-07-31 23:09:54 -0600

    Update GPTMilestonesAnalysis.R

--- GPTMilestonesAnalysis.R
+++ GPTMilestonesAnalysis.R
@@ -12,9 +12,9 @@ library(ggplot2)
 ggplot(data = aja, aes(x = Date, y = Sentences)) +
   geom_line() + geom_point() + theme_bw() + 
   scale_x_date(date_labels = "%Y-%m", date_breaks = "1 month") +
-  theme(axis.text.x = element_text(size = 15, color = "black"),  
-        axis.text.y = element_text(size = 15, color = "black"),  
-        axis.title.x = element_text(size = 15, color = "black"), 
-        axis.title.y = element_text(size = 15, color = "black")) 
+  theme(axis.text.x = element_text(size = 12, color = "black"),  
+        axis.text.y = element_text(size = 12, color = "black"),  
+        axis.title.x = element_text(size = 13, color = "black"), 
+        axis.title.y = element_text(size = 13, color = "black")) 
 
-ggsave("Figure2.pdf", dpi = "print", width = 21, height = 15, units = "cm")
+ggsave("Figure2.pdf", dpi = "print", width = 22, height = 15, units = "cm")
