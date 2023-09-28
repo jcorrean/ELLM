689d9163dab64c1395ae5696d4d6431126eb1ed3
Juan C. Correa <j.correa.n@gmail.com>
2023-09-27 23:35:03 -0600

    Update Centralities.R

--- Centralities.R
+++ Centralities.R
@@ -15,3 +15,22 @@ Centralities <- Centralities[1:4]
 Centralities <- Centralities[order(-Centralities$Eigen.vector.vector), ]
 colnames(Centralities)[4] <- "eigenvector"
 Centralities$Nodes <- rownames(Centralities)
+#https://github.com/davidsjoberg/ggbump
+
+
+ggplot(df, aes(year, rank, color = country)) +
+   geom_point(size = 7) +
+   geom_text(data = df %>% filter(year == min(year)),
+             aes(x = year - .1, label = country), size = 5, hjust = 1) +
+   geom_text(data = df %>% filter(year == max(year)),
+             aes(x = year + .1, label = country), size = 5, hjust = 0) +
+   geom_bump(size = 2, smooth = 8) +
+   scale_x_continuous(limits = c(2010.6, 2013.4),
+                      breaks = seq(2011, 2013, 1)) +
+   theme_minimal_grid(font_size = 14, line_size = 0) +
+   theme(legend.position = "none",
+         panel.grid.major = element_blank()) +
+   labs(y = "RANK",
+        x = NULL) +
+   scale_y_reverse() +
+   scale_color_manual(values = wes_palette(n = 4, name = "GrandBudapest1"))
