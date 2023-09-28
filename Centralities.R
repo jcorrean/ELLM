4d167daa22e0fbfd30aaafeb39f47194a9194df8
Juan C. Correa <j.correa.n@gmail.com>
2023-09-28 17:13:09 -0600

    Update Centralities.R

--- Centralities.R
+++ Centralities.R
@@ -23,11 +23,13 @@ Centralities <- Centralities[c(1,6,7)]
 library(ggplot2)
 library(ggbump)
 library(ggrepel)
+custom_labels <- c("December 2022", "January 2023", "February 2023", "March 2023", "April 2023", "May 2023")
+# Modify the ggplot code to use custom labels
 ggplot(Centralities, aes(Month, Rank, color = Nodes)) +
    geom_bump(size = 2) +
    geom_text_repel(aes(label = Nodes), nudge_y = 0.2, size = 4) +  # Add non-overlapping labels
-   theme_bw() +
-   scale_x_continuous(breaks = unique(Centralities$Month)) +
+   theme_cowplot() +
+   scale_x_continuous(breaks = 1:6, labels = custom_labels) +  # Use custom labels
    scale_y_reverse(breaks = 1:10, labels = 1:10) +  # Reverse the Y-axis and set breaks/labels from 1 to 10
-   labs(x = "Months", y = "Rank") +
+   labs(x = "First six months of Wikipedia edits for ChatGPT", y = "Rank") +
    theme(legend.position = "none")
