349beabc0d6da1beacea534834da3b50f560d872
Juan C. Correa <j.correa.n@gmail.com>
2023-10-01 23:07:52 -0600

    Update SubNetworks.R

--- SubNetworks.R
+++ SubNetworks.R
@@ -14,8 +14,30 @@ May  <- dplyr::filter(Network, grepl("May 2023",Month))
 
 library(igraph)
 December <- as.matrix(December)
+January <- as.matrix(January)
+February <- as.matrix(February)
+March <- as.matrix(March)
+April <- as.matrix(April)
+May <- as.matrix(May)
+
 net1 <- graph_from_edgelist(December, directed = FALSE)
+net2 <- graph_from_edgelist(January, directed = FALSE)
+net3 <- graph_from_edgelist(February, directed = FALSE)
+net4 <- graph_from_edgelist(March, directed = FALSE)
+net5 <- graph_from_edgelist(April, directed = FALSE)
+net6 <- graph_from_edgelist(May, directed = FALSE)
+
+
 summary(net1)
+summary(net2)
+summary(net3)
+summary(net4)
+summary(net5)
+summary(net6)
+
+
+par(mfrow = c(3, 2))
+
 plot(net1, 
      displaylabels = TRUE, 
      edge.width=0.5,
@@ -26,3 +48,52 @@ plot(net1,
      vertex.label.font=0.5, 
      layout =   layout_components)
 
+plot(net2, 
+     displaylabels = TRUE, 
+     edge.width=0.5,
+     vertex.label.cex=0.6,
+     vertex.shape = "none",
+     vertex.color = "white",
+     vertex.frame.color = "gray",
+     vertex.label.font=0.5, 
+     layout =   layout_components)
+
+plot(net3, 
+     displaylabels = TRUE, 
+     edge.width=0.5,
+     vertex.label.cex=0.6,
+     vertex.shape = "none",
+     vertex.color = "white",
+     vertex.frame.color = "gray",
+     vertex.label.font=0.5, 
+     layout =   layout_components)
+
+plot(net4, 
+     displaylabels = TRUE, 
+     edge.width=0.5,
+     vertex.label.cex=0.6,
+     vertex.shape = "none",
+     vertex.color = "white",
+     vertex.frame.color = "gray",
+     vertex.label.font=0.5, 
+     layout =   layout_components)
+
+plot(net5, 
+     displaylabels = TRUE, 
+     edge.width=0.5,
+     vertex.label.cex=0.6,
+     vertex.shape = "none",
+     vertex.color = "white",
+     vertex.frame.color = "gray",
+     vertex.label.font=0.5, 
+     layout =   layout_components)
+
+plot(net6, 
+     displaylabels = TRUE, 
+     edge.width=0.5,
+     vertex.label.cex=0.6,
+     vertex.shape = "none",
+     vertex.color = "white",
+     vertex.frame.color = "gray",
+     vertex.label.font=0.5, 
+     layout =   layout_components)
