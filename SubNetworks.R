6c1cdb58a0d8f738dfd296cd0e16186fb2e0b0ec
Juan C. Correa <j.correa.n@gmail.com>
2023-10-01 22:49:22 -0600

    Create SubNetworks.R

---
+++ SubNetworks.R
@@ -0,0 +1,24 @@
+rm(list = ls())
+load("SelectedKeywords.RData")
+df$keyword <- tolower(df$keyword)
+Network <- df[,c(7,8)]
+Network <- Network[!duplicated(Network), ]
+Network <- Network[,c(ncol(Network),1:(ncol(Network)-1))]
+
+December <- dplyr::filter(Network, grepl("December 2022",Month))
+
+
+library(igraph)
+December <- as.matrix(December)
+net1 <- graph_from_edgelist(December, directed = FALSE)
+summary(net1)
+plot(net1, 
+     displaylabels = TRUE, 
+     edge.width=0.5,
+     vertex.label.cex=0.6,
+     vertex.shape = "none",
+     vertex.color = "white",
+     vertex.frame.color = "gray",
+     vertex.label.font=0.5, 
+     layout =   layout_components)
+
