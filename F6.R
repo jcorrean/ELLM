baaa29fdbba6a6324801eef83efda7547c976960
Juan C. Correa <j.correa.n@gmail.com>
2023-10-05 11:27:59 -0600

    Update F6.R

--- F6.R
+++ F6.R
@@ -4,10 +4,39 @@ df$keyword <- tolower(df$keyword)
 Network <- df[,c(7,8)]
 terms <- data.frame(table(Network$pattern))
 library(dplyr)
+# concern refers to terms that suggest 
+# (un)desirable usages
 Network$Category <- Network %>%
    mutate(category = case_when(
-      terms %in% c("hallucinat", "jobs", "") ~ "concern",
-      terms %in% c("prototype", "api", "") ~ "roles",
-      terms %in% c("ai", "information", "knowledge", ) ~ "generic use",
+      terms %in% c("hallucinat", "jobs",
+                   "racist", "sexist",
+                   "bias", "factual accuracy",
+                   "plagiarism", "jailbreak",
+                   "risk", "cybersecurity",
+                   "academic plagiarism",
+                   "significant drawback",
+                   "limited knowledge",
+                   "") ~ "concern",
+      terms %in% c("prototype", "api",
+      "prompt", "InstructGPT",
+      "architecture", "algorithmic bias",
+      "artificial intelligence", "language model",
+      "machine learning", "reinforcement learning",
+      "proximal policy optimization",
+      "reward models", "supervised learning",
+      "generative pre-trained transformer",
+      "large language models",
+      "algorithm") ~ "roles",
+      terms %in% c("ai", "information",
+      "knowledge", "human-like",
+      "usability", "chatbot", "data",
+      "goodhart's law", "detailed responses",
+      "human trainers", "chat",
+      "virtual assistant",
+      "microsoft azure",
+      "Meta", "robotics",
+      "SearchGPT","bing chat",
+      "Google Bard", "Bing Chat",
+      "") ~ "generic use",
       TRUE ~ NA_character_  # Default case, in case a term doesn't match any category
    ))
